b376bb680c540ca0d70e345d6086044710ea2bef
Henry Addison <henryaddison@users.noreply.github.com>
2022-10-13 15:57:30 +0100

    by default skip sampling during training

--- configs/default_xarray_configs.py
+++ configs/default_xarray_configs.py
@@ -14,7 +14,7 @@ def get_default_configs():
   ## store additional checkpoints for preemption in cloud computing environments
   training.snapshot_freq_for_preemption = 1000
   ## produce samples at each snapshot.
-  training.snapshot_sampling = True
+  training.snapshot_sampling = False
   training.likelihood_weighting = False
   training.continuous = True
   training.reduce_mean = False
