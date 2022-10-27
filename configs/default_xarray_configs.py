d8567576a1604aec033da48abe44f637a434af14
Henry Addison <henryaddison@users.noreply.github.com>
2022-10-27 08:18:30 +0100

    take fewer snapshots
    only using the last one at the moment anyway

--- configs/default_xarray_configs.py
+++ configs/default_xarray_configs.py
@@ -8,7 +8,7 @@ def get_default_configs():
   config.training = training = ml_collections.ConfigDict()
   config.training.batch_size = 16#128
   training.n_iters = 100000
-  training.snapshot_freq = 5000
+  training.snapshot_freq = 25000
   training.log_freq = 50
   training.eval_freq = 100
   ## store additional checkpoints for preemption in cloud computing environments
