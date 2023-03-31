fb408549617710e239fc4704cdeab7e4bc28e8d8
Henry Addison <henryaddison@users.noreply.github.com>
2023-03-31 15:04:46 +0100

    update number of channels in nets to include time

--- src/ml_downscaling_emulator/score_sde_pytorch_hja22/models/cncsnpp.py
+++ src/ml_downscaling_emulator/score_sde_pytorch_hja22/models/cncsnpp.py
@@ -140,8 +140,9 @@ class cNCSNpp(nn.Module):
       raise ValueError(f'resblock type {resblock_type} unrecognized.')
 
     # Downsampling block
-    cond_channels, output_channels = list(map(len, get_variables(config.data.dataset_name)))
-    channels = cond_channels + output_channels + config.model.map_features + config.model.loc_spec_channels
+    cond_var_channels, output_channels = list(map(len, get_variables(config.data.dataset_name)))
+    cond_time_channels = 3
+    channels = cond_var_channels + cond_time_channels + output_channels + config.model.map_features + config.model.loc_spec_channels
     if progressive_input != 'none':
       input_pyramid_ch = channels
 
