4f9c2a3c33cb281235572b4230d6796bb26dafac
Henry Addison <henryaddison@users.noreply.github.com>
2023-06-21 14:37:39 +0100

    update deterministic restore_checkpoint to be the same as diffusion version

--- src/ml_downscaling_emulator/deterministic/utils.py
+++ src/ml_downscaling_emulator/deterministic/utils.py
@@ -8,16 +8,20 @@ def restore_checkpoint(ckpt_dir, state, device):
     if not os.path.exists(ckpt_dir):
         os.makedirs(os.path.dirname(ckpt_dir), exist_ok=True)
         logging.warning(
-            f"No checkpoint found at {ckpt_dir}. " f"Returned the same state as input"
+            f"No checkpoint found at {ckpt_dir}." f"Returned the same state as input"
         )
-        return state
+        return state, False
     else:
         loaded_state = torch.load(ckpt_dir, map_location=device)
         state["optimizer"].load_state_dict(loaded_state["optimizer"])
         state["model"].load_state_dict(loaded_state["model"], strict=False)
         state["step"] = loaded_state["step"]
         state["epoch"] = loaded_state["epoch"]
-        return state
+        logging.info(
+            f"Checkpoint found at {ckpt_dir}. "
+            f"Returned the state from {state['epoch']}/{state['step']}"
+        )
+        return state, True
 
 
 def save_checkpoint(ckpt_dir, state):
