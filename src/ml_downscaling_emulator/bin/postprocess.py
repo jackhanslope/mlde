ff85a876b4f6f5e104d3c854a8fb67dc5897a1da
Henry Addison <henryaddison@users.noreply.github.com>
2023-09-11 13:11:39 +0100

    change filtering logging to check the dir being scanned

--- src/ml_downscaling_emulator/bin/postprocess.py
+++ src/ml_downscaling_emulator/bin/postprocess.py
@@ -44,19 +44,17 @@ def filter(
     )
     os.makedirs(filtered_samples_dirpath, exist_ok=False)
 
-    samples_filepaths_to_filter = samples_glob(
-        samples_path(
-            workdir,
-            checkpoint=checkpoint,
-            input_xfm=input_xfm,
-            dataset=dataset,
-            split=split,
-            ensemble_member=ensemble_member,
-        )
+    samples_filepaths_to_filter = samples_path(
+        workdir,
+        checkpoint=checkpoint,
+        input_xfm=input_xfm,
+        dataset=dataset,
+        split=split,
+        ensemble_member=ensemble_member,
     )
 
     logger.info(f"Found for filtering: {samples_filepaths_to_filter}")
-    for sample_filepath in samples_filepaths_to_filter:
+    for sample_filepath in samples_glob(samples_filepaths_to_filter):
         logger.info(f"Working on {sample_filepath}")
         samples_ds = xr.open_dataset(sample_filepath)
 
